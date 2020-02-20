8d596d44b6dc9eee501f57cfa2440ff59fde327b
quelmax <quemuelvital@live.com>
2020-02-20 15:55:32 -0300

    making independent functions

--- Exercicio_5.py
+++ Exercicio_5.py
@@ -2,19 +2,31 @@ import math
 
 pi = math.pi
 
-i = 0
+def entrada_usuario():
+    i = 0
+    while i == 0:
+        try:
+            r = float(input('Digite um raio: '))
+        except ValueError:
+            print ('\nVocê não digitou um número, por favor entre com um número\n')
+        else: 
+            i = 1
+    return r
 
-while i == 0:
-  try:
-      r = float(input('Digite um raio: '))
-  except ValueError:
-      print ('\nVocê não digitou um número, por favor entre com um número\n')
-  else: 
-    i = 1
-  
-area = pi * (r * r)
-comp = 2 * (pi * r)
+def calculo_area(r):
+    area = pi * (r * r)
+    return area
 
+def calculo_comprimento(r):
+    comp = 2 * (pi * r)
+    return comp
+
+r = entrada_usuario()
+area = calculo_area(r)
+comp = calculo_comprimento(r)
 print("\nA area de uma circunferência com raio",r, "é de", area, ",e o comprimento é de {}.".format(comp))
+
+
+
 
 
